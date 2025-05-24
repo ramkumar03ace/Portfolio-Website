98bc271e9781185edb8dd7d5d58d70cdd79423ed
ramkumar03ace <ramkumar.arcot2022@vitstudent.ac.in>
2025-05-24 23:36:04 +0530

    added meme line

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -150,7 +150,7 @@ export const emacs = async (args?: string[]): Promise<string> => {
 
 export const sudo = async (args?: string[]): Promise<string> => {
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `Permission denied: with little power comes... no responsibility? `;
+  return `Access denied. But at least you get a... musical surprise. 🎶 `;
 };
 
 // Banner
