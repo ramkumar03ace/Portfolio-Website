4645a3fd6b4be1cdedccdbe794fd8b13924207ce
ramkumar03ace <ramkumar.arcot2022@vitstudent.ac.in>
2025-05-24 14:55:03 +0530

    added all the intials files and completed 40%

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+    return (
+        <Html className="dark">
+            <Head>
+                
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
