10dab0aed91b67561432e8b002b9482f3f86d241
ramkumar03ace <ramkumar.arcot2022@vitstudent.ac.in>
2025-05-25 18:02:05 +0530

    minor spell mistake change

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -160,7 +160,7 @@ Wellcome To My Portfolio Website!
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
-Typr 'about' to know more about me.
+Type 'about' to know more about me.
 
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 
